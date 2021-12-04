4c79205aa06f2891e8f286fdfdceb6884e791d8b
Ansar <ansarmirzayi@gmail.com>
2021-12-04 23:39:38 +0330

    create Products Model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory,SoftDeletes,Sluggable;
+
+    protected $fillable=[
+        'title',
+        'slug',
+        'description',
+        'price',
+        'images',
+        'category_id',
+        'viewCount',
+        'orderCount',
+        'likeCount',
+        'order_number',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function category(){
+        return $this->hasOne(Category::class);
+    }
+}
---
+++ database/migrations/2021_12_04_200124_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('price');
+            $table->unsignedInteger('orderCount')->default(0);
+            $table->unsignedInteger('viewCount')->default(0);
+            $table->unsignedInteger('likeCount')->default(0);
+            $table->unsignedBigInteger('category_id')->default(0);
+            $table->text('images')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
